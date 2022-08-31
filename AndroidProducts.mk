7efe9c8a40801f333772d88bdaf64bfe8afab40f
黄豆酱 <53000648+e5u@users.noreply.github.com>
2022-08-31 09:58:29 +0800

    initial commits

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_OP46F1.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_OP46F1-user \
+    omni_OP46F1-userdebug \
+    omni_OP46F1-eng
